11bdc1d839d4c7d60fb1aef78a231a463af5f80c
GretaRob <greta.robichaud@gmail.com>
2021-02-15 19:27:39 +0700

    edited

--- index.js
+++ index.js
@@ -109,3 +109,14 @@ if (glide)
       },
     },
   }).mount();*/
+
+
+  function myFunction() {
+    var x = document.getElementById("gif-click");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
