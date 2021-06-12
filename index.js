0cf4fac7ac0acc276c572facb78b76c104863776
GretaRob <greta.robichaud@gmail.com>
2021-06-12 21:22:51 +0700

    added a new project

--- index.js
+++ index.js
@@ -146,4 +146,19 @@ window.onclick = function(event) {
     modal3.style.display = "none";
   }
 }
-    
+
+//zero project gif
+var modal0 = document.getElementById("myModal0");
+var btn0 = document.getElementById("myBtn0");
+var span0 = document.getElementsByClassName("close0")[0];
+btn0.onclick = function() {
+  modal0.style.display = "block";
+}
+span0.onclick = function() {
+  modal0.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal0.style.display = "none";
+  }
+}
